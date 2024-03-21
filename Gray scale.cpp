db384ea5ea6373c49b2e16722d2f2d42acc49cde
Ibrahim Mohamed <141458738+QNQ124@users.noreply.github.com>
2024-03-22 01:23:45 +0200

    Update and rename main.cpp to Gray scale.cpp

--- Gray scale.cpp
+++ Gray scale.cpp
@@ -1,5 +1,5 @@
 #include <bits/stdc++.h>
-#include "Image_class.h"
+#include "Image_class.h"  
 using namespace std;
 
 
@@ -9,7 +9,7 @@ int main(){
 
 
     for (int i = 0; i < f.width; ++i){
-        for (int j = 0; j < f.height; ++j){
+        for (int j = 0; j < f.height; ++j){ 
 
             unsigned int average = 0;
 
