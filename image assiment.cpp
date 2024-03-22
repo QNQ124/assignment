fac769223536a5a9e28569de170daa2c88df9d30
ezzeldin2005 <160146914+ezzeldin2005@users.noreply.github.com>
2024-03-22 16:49:45 +0200

    Update image assiment.cpp ( added rotate function)

--- image assiment.cpp
+++ image assiment.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <string>
 #include <cmath>
-#include "Image_Class.h" // Assuming this header contains the Image class declaration
+#include "Image_Class.h"
 using namespace std;
 
 void Inverted_Image()
@@ -51,9 +51,12 @@ void Blur_image()
     int kernel_size = 5; // Increased kernel size
     int num_passes = 5; // Number of passes for blurring
 
-    for (int pass = 0; pass < num_passes; pass++) {
-        for (int i = 0; i < photo.width; i++) {
-            for (int j = 0; j < photo.height; j++) {
+    for (int pass = 0; pass < num_passes; pass++)
+    {
+        for (int i = 0; i < photo.width; i++)
+        {
+            for (int j = 0; j < photo.height; j++)
+            {
                 double weighted_sum[3] = {0.0}; // Initialize array for each channel
                 double sum_of_weights = 0.0;
 
@@ -75,7 +78,8 @@ void Blur_image()
                     }
                 }
                 // Assign the blurred pixel values back to the image
-                for (int c = 0; c < 3; c++) {
+                for (int c = 0; c < 3; c++)
+                {
                     photo.setPixel(i, j, c, weighted_sum[c] / sum_of_weights);
                 }
             }
@@ -84,10 +88,78 @@ void Blur_image()
     // Save the blurred image
     photo.saveImage(Newfile_name);
 }
+void rotate_image()
+{
+    string file_name , Newfile_name;
+    string degree_rotation;
+    cout<<"Please enter the file name: "<<endl;
+    cin>>file_name;
+    cout<<"Please enter the NEW file name: "<<endl;
+    cin>>Newfile_name;
+    cout<<"Please enter the degree_rotation(90 or 270 or 180): "<<endl;
+    cin>>degree_rotation;
 
+    Image photo(file_name);
+
+    while(degree_rotation != "90" && degree_rotation != "180" && degree_rotation != "270")
+
+    {
+        cout<<"invaild input,Please enter the degree_rotation(90 or 270 or 180): "<<endl;
+        cin>>degree_rotation;
+    }
+
+    if(degree_rotation=="180")
+    {
+        Image new_photo_180(photo.width,photo.height);
+        for(int i = 0; i < photo.width; i++) // Using .width  to get image width
+        {
+            for (int j = 0; j < photo.height; j++) // Using .height to get image height
+            {
+                for (int k = 0; k < 3; k++) // Assuming 3 color channels (RGB)
+                {
+                    new_photo_180(photo.width-i-1,photo.height-j-1,k)=photo(i,j,k);
+                }
+            }
+        }
+        new_photo_180.saveImage(Newfile_name);
+    }
+    else if (degree_rotation=="270")
+    {
+        Image new_photo_270(photo.height,photo.width);
+
+        for (int i = 0; i < photo.width; i++)
+        {
+            for (int j = 0; j < photo.height; j++)
+            {
+                for (int k = 0; k < 3; k++)
+                {
+                    new_photo_270(j, photo.width - i - 1, k) = photo(i, j, k);
+                }
+            }
+        }
+
+        new_photo_270.saveImage(Newfile_name);
+    }
+    else
+    {
+        Image new_photo_90(photo.height,photo.width);
+
+        for (int i = 0; i < photo.width; i++)
+        {
+            for (int j = 0; j < photo.height; j++)
+            {
+                for (int k = 0; k < 3; k++)
+                {
+                    new_photo_90(photo.height - j - 1, i, k) = photo(i, j, k);
+                }
+            }
+        }
+
+        new_photo_90.saveImage(Newfile_name);
+    }
+}
 
 int main()
 {
-
-    Blur_image();
+    rotate_image();
 }
